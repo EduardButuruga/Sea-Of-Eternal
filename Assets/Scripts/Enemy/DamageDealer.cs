92844cb8663b948bddfa9552d4aa30f994aadb98
EduardButuruga <160519389+EduardButuruga@users.noreply.github.com>
2024-05-17 01:41:58 +0300

    PlayerHealth
    Baboi

---
+++ Assets/Scripts/Enemy/DamageDealer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    public int damage = 1; // Cantitatea de damage dată jucătorului
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected with: " + collision.gameObject.name); // Log când se detectează coliziunea
+
+        if (collision.CompareTag("Player")) // Verifică dacă obiectul colizionat este jucătorul
+        {
+            Debug.Log("Collision with Player detected."); // Log când se colizionează cu jucătorul
+
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                Debug.Log("PlayerHealth component found on Player."); // Log când componenta PlayerHealth este găsită
+
+                playerHealth.TakeDamage(damage, transform.position);
+            }
+            else
+            {
+                Debug.LogWarning("PlayerHealth component not found on Player."); // Log de avertizare dacă componenta PlayerHealth nu este găsită
+            }
+        }
+        else
+        {
+            Debug.Log("Collision with non-Player object."); // Log când coliziunea nu este cu jucătorul
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+
+    public HealthBar healthBar;
+    private Animator animator;
+
+    public static PlayerHealth Instance { get; private set; }
+    private bool isDead;
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject); // Optional: Keep the player across scenes
+        }
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    public void TakeDamage(int damage, Vector3 enemypos)
+    {
+        health -= damage;
+        healthBar.SetHealth(health);
+        if (animator != null)
+        {
+            animator.SetTrigger("hurt");
+        }
+
+        if (health <= 0 && !isDead)
+        {
+            isDead = true;
+            // Poți adăuga logică pentru când jucătorul moare, de exemplu reîncărcarea scenei
+            Debug.Log("Player is dead!");
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Decomentariu pentru a reîncărca scena
+        }
+    }
+}
